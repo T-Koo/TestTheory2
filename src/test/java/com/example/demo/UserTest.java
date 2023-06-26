b9f9cf409587bd83dcccff9d363fa1f78468a232
T-Koo <te200783@gmail.com>
2023-06-27 08:36:58 +0900

    課題3途中

--- src/test/java/com/example/demo/UserTest.java
+++ src/test/java/com/example/demo/UserTest.java
@@ -1,5 +1,49 @@
 package com.example.demo;
 
-public class UserTest {
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+	static User user = null;
+	@BeforeAll
+	static void beforeTest() {
+		user = new User(null);
+	}
+	
+	@Test
+	void 正常系_ユーザー管理コード登録参照() {
+		user.setCode("1234");
+		assertThat("1234").isEqualTo(user.getCode());
+	}
+	
+	@Test
+	void 正常系_名前登録参照() {
+		user.setName("東北タロウ");
+		assertThat("東北タロウ").isEqualTo(user.getName());
+	}
+	
+	@Test
+	void 正常系_年齢登録参照() {
+		user.setAge(21);
+		assertThat(21).isEqualTo(user.getAge());
+	}
+	
+	@Test
+	void 異常系_範囲外年齢登録() {
+		user.setAge(-20);
+		assertThat(-1).isEqualTo(user.getAge());
+	}
+	
+	@Test
+	void 異常系_年齢() {
+		
+	}
+	
+	@AfterAll
+	static void afterTest() {
+		user = null;
+	}
 }
